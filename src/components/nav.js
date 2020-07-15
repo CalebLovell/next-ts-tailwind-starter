39b6e0ce9b48a68bb3d0852a3f046f836805fd33
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2020-07-15 01:34:07 -0600

    run formatting

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,8 +1,8 @@
 import Link from 'next/link';
 
 const links = [
-	{ href: 'https://github.com/vercel/next.js', label: 'GitHub' },
-	{ href: 'https://nextjs.org/docs', label: 'Docs' },
+	{ href: `https://github.com/vercel/next.js`, label: `GitHub` },
+	{ href: `https://nextjs.org/docs`, label: `Docs` },
 ];
 
 export default function Nav() {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import Nav from '../../components/nav';
+import Nav from '@components/nav';
 
 export default function IndexPage() {
 	return (
@@ -6,6 +6,10 @@ export default function IndexPage() {
 			<Nav />
 			<div className='hero'>
 				<h1 className='title'>Next.js + Tailwind CSS</h1>
+			</div>
+			<div className='container mx-auto'>
+				<h1 className='text-lg text-center m-4'>TailwindUI/Next.js</h1>
+				<p className='bg-green-600'>This is a test of the tailwind next integration.</p>
 			</div>
 		</div>
 	);
